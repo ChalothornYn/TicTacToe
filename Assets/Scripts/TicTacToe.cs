f3f0aa7bd29f41823c7ac48f80702fe82ed74a20
Chalothorn Yooyuen <chalothorn.yn@gmail.com>
2022-06-07 12:00:06 +0700

    Add board array

--- Assets/Scripts/TicTacToe.cs
+++ Assets/Scripts/TicTacToe.cs
@@ -6,27 +6,80 @@ public class TicTacToe : MonoBehaviour
 {
     private Collider2D[] _grid = new Collider2D[9];
 
+    [SerializeField] private string player1Symbol;
+    [SerializeField] private string player2Symbol;
+
+    [Space] [SerializeField] private bool playAsPlayer1 = true;
+
+    [Space]
+    [SerializeField] private int boardWidth;
+    [SerializeField] private int boardHigh;
+
+    private string[ , ] _board;
+
+    private enum State
+    {
+        Rest,
+        Player1Turn,
+        Player2Turn,
+        CheckResult,
+    }
+
+    private State _stateFlow = State.Rest;
+    
     private void Start()
     {
         _grid = GetComponentsInChildren<Collider2D>();
+        
+        _board = new string[boardWidth, boardHigh];
+        
+        for (var i = 0; i < boardWidth; i++)
+        {
+            for (var j = 0; j < boardHigh; j++)
+            {
+                _board[i, j] = "";
+            }
+        }
     }
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        switch (_stateFlow)
         {
-            Vector2 mousePosition = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
-            
-            foreach (var col in _grid)
-            {
-                if (col.OverlapPoint(mousePosition))
-                {
-                    Debug.Log($"<color=red>{col.name}</color>");
-                    break;
-                }
-            }
+            case State.Rest:
+                // Choose Game Mode
+                if(playAsPlayer1)
+                    _stateFlow = State.Player1Turn;
+                break;
+            case State.Player1Turn:
+                
+                break;
+            case State.Player2Turn:
+                break;
+            case State.CheckResult:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
         }
+        
+
+
+        // if (Input.GetMouseButton(0))
+        // {
+        //     Vector2 mousePosition = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
+        //     
+        //     foreach (var col in _grid)
+        //     {
+        //         if (col.OverlapPoint(mousePosition))
+        //         {
+        //             Debug.Log($"<color=red>{col.name}</color>");
+        //             break;
+        //         }
+        //     }
+        // }
     }
+    
+    
 
     private void OnMouseDown()
     {
